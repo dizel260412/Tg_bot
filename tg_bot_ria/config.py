fc2df1cc79bc1551f8510ade68c6104796e28068
dizel260412 <71236409+dizel260412@users.noreply.github.com>
2022-08-12 09:01:46 +0300

    Update config.py

--- tg_bot_ria/config.py
+++ tg_bot_ria/config.py
@@ -1,5 +1,5 @@
-user_id = 1797102351
-token = '1765679473:AAEFSR7oCU3VsaWNpJeX9PFd-IAT-dFNCJw'
+user_id = 
+token = ''
 headers = {
         "user-agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 99.0.4844.51 Safari / 537.36"
     }
